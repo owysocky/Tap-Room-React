008c9cfd54bb80c5b8585a8d7d17609fed3d941f
ow <volovykolya@gmail.com>
2019-04-26 08:58:31 -0700

    create initial file

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+function App(){
+  return (
+    <div>
+    
+      <Header/>
+      <Switch>
+
+      </Switch>
+    
+  );
+}
+
+export default App;
