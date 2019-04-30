76a9a56548e45451c063924fc01f041dc1497737
ow <volovykolya@gmail.com>
2019-04-29 18:07:11 -0700

    add hardcoded beerList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,16 +13,58 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      onTap: []
+      onTap: [
+        {
+          name: "Kentucky Brunch Brand Stout",
+          type: "American Imperial Stout",
+          company: "Toppling Goliath Brewing Co.",
+          alcohol: "12.00",
+          price: "5"
+        },
+        {
+          name: "Marshmallow Handjee",
+          type: "Russian Imperial Stout",
+          company: "3 Floyds Brewing Co.",
+          alcohol: "15.00",
+          price: "5"
+        },
+        {
+          name: "Barrel-Aged Abraxas",
+          type: "American Imperial Stout",
+          company: "Perennial Artisan Ales",
+          alcohol: "11.00",
+          price: "5"
+        },
+        {
+          name: "Hunahpu's Imperial Stout",
+          type: "American Imperial Stout",
+          company: "Cigar City Brewing",
+          alcohol: "11.00",
+          price: "5"
+        },
+        {
+          name: "King Julius",
+          type: "New England IPA",
+          company: "Tree House Brewing Company",
+          alcohol: "8.30",
+          price: "5"
+        },
+        {
+          name: "Heady Topper",
+          type: "New England IPA",
+          company: "The Alchemist",
+          alcohol: "8.00",
+          price: "5"
+        }
+      ]
     };
     this.handleAddingNewBeer = this.handleAddingNewBeer.bind(this);
   }
 
   handleAddingNewBeer(newBeer) {
     var newOnTapList = this.state.onTap.slice();
-    newOnTapList.push(newBeer);
+    newOnTapList.unshift(newBeer);
     this.setState({ onTap: newOnTapList });
-    console.log(this.state.onTap);
   }
 
   render() {
