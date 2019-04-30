8dd366675dd393663a039c44287d1f3f1608d2c2
ow <volovykolya@gmail.com>
2019-04-29 17:05:17 -0700

    make App a class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,18 @@ import BeerList from './BeerList';
 import AddBeer from './AddBeer';
 import ErrorHandler from './ErrorHandler';
 
-function App(){
-  return (
-    <div>
-      <style global>{`
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onTap: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <style global>{`
         div{
           font-family: Courier New;
         }
@@ -20,20 +28,21 @@ function App(){
           margin-bottom: 70px;
         }
       `}</style>
-      <div className="footer-margin">
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/locations' component={Locations}/>
-        <Route path='/beer' component={BeerList}/>
-        <Route path='/admin' component={AddBeer}/>
-        <Route component={ErrorHandler} />
-      </Switch>
-      </div>
-      <Footer/>
+        <div className="footer-margin">
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/locations' component={Locations} />
+            <Route path='/beer' component={BeerList} />
+            <Route path='/admin' component={AddBeer} />
+            <Route component={ErrorHandler} />
+          </Switch>
+        </div>
+        <Footer />
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
